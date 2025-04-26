2c7ceb672464be37f0be18a3dc34e581bc05ef8b
Tiasa24 <tiasamalitya@gmail.com>
2025-04-26 02:09:40 +0100

    Delete GYM.Database.file.sql

--- GYM.Database.file.sql
+++
@@ -1,7 +0,0 @@
-SELECT Membership_Type, COUNT(MemberId) AS Total_Members
-FROM Membership_Type 
-JOIN Member ON Member.MembershipId = Membership_Type.MembershipId
-GROUP BY Membership_Type
-ORDER BY Total_Members DESC;
-
-
